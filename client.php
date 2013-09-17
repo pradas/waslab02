3e7a0957b5caccce4729059184c606aababf86e7
grau_asw <farre@essi.upc.edu>
2013-09-17 21:46:53 +0000

    uri path fixed

--- client.php
+++ client.php
@@ -1,6 +1,6 @@
 <?php
 
-$URI = 'http://localhost:8080/aswlab02x/wall.php';
+$URI = 'http://localhost:8080/aswlab021/wall.php';
 $resp = file_get_contents($URI);
 echo $http_response_header[0], "\n"; // Print the first HTTP response header
 echo $resp;  // Print HTTP response body
